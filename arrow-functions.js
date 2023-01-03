e1627a9744e3d8dd4cc2eae0f6314547e552e7d7
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-01-03 02:08:18 +0000

    added arrow functions syntaxs

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,40 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwonumbers(a,b){
+//     return a+b;
+// }
+// let sum= addTwonumbers(3,4);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwonumbers=(a,b) =>{
+    return a+b;
+}
+let sum= addTwonumbers(3,4);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+//const addTwonumbers2=(a,b)=>(a+b); Also valid
+
+const addTwonumbers2=(a,b)=>a+b;
+let sum2=addTwonumbers2(5,6)
+console.log(sum2);
 
 // Implicit Returns
 
+const saysomthing =massage => console.log(messagr);
+saysomthing("Hello world");
+
+const sayHi =() => console.log('Hiiiiiiiii')
+sayHi();
 // Returning Multiple Lines
+const returnMultilines = () => {
+    <p>
+        '
+            This is a Multiple line code
+        '
+    </p>
+}
+console.log(returnMultilines);
+
